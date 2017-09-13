3d11a65bc2264e331105fe11c2021eba3036cce1
Martin Midolesov <mmidolesov@vmware.com>
2017-09-13 13:21:54 +0300

    Init
    Initial commit

---
+++ src/main/java/com/mmidolesov/Application.java
@@ -0,0 +1,15 @@
+package com.mmidolesov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.mmidolesov"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/resources/templates/test.html
@@ -0,0 +1,1 @@
+<p>test new</p>
